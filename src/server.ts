fb0693b8b2fe4e7f6f762eec5c2e0b5741b43389
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-09-15 20:26:22 -0300

    Cria testa e confirma o funcionamento da rota de upload

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
 // Registra a rota por meio de uma função importada
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 app.listen({
   port: 3333,
